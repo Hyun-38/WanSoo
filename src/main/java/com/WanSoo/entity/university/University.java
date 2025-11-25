46eefbc5245203bfd52221b46d7fd7c212faad47
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:29:06 +0900

    Feat(#1) University 엔티티 작성

--- src/main/java/com/WanSoo/entity/university/University.java
+++ src/main/java/com/WanSoo/entity/university/University.java
@@ -1,4 +1,22 @@
 package com.WanSoo.entity.university;
 
-public class University {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class University { // 대학교 관련 엔티티 ( 대학 정보 )
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String region;
+
 }
