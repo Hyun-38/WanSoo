b60a40b172c3e8b04e23705b97cc55046a49c5fe
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:29:44 +0900

    Feat(#1) UserScore 엔티티 작성

--- src/main/java/com/WanSoo/entity/user/UserScore.java
+++ src/main/java/com/WanSoo/entity/user/UserScore.java
@@ -1,4 +1,28 @@
 package com.WanSoo.entity.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
 public class UserScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double korean;
+    private double math;
+    private double english;
+    private double social;
+    private double science;
+    private double history;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
