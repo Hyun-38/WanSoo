71d8f27495bd79b1965a42b2dd51c2b61d725885
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:30:20 +0900

    Feat(#1) Department 엔티티 작성

--- src/main/java/com/WanSoo/entity/university/Department.java
+++ src/main/java/com/WanSoo/entity/university/Department.java
@@ -1,4 +1,28 @@
 package com.WanSoo.entity.university;
 
-public class Department {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Department { // 학과 관련 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    // 가군/나군/다군
+    @Column(nullable = false)
+    private String admissionGroup;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id")
+    private University university;
+
 }
