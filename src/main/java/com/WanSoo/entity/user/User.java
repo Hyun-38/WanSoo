679e2f005716a2f792fbed098740926f4baf341e
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:29:27 +0900

    Feat(#1) User 엔티티 작성

--- src/main/java/com/WanSoo/entity/user/User.java
+++ src/main/java/com/WanSoo/entity/user/User.java
@@ -1,4 +1,23 @@
 package com.WanSoo.entity.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String password;
+
 }
