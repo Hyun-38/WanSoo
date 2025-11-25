92406e51f0fd93a9412282226de85ac1e74ba9ce
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:29:55 +0900

    Feat(#1) ScoreWeight 엔티티 작성

--- src/main/java/com/WanSoo/entity/university/ScoreWeight.java
+++ src/main/java/com/WanSoo/entity/university/ScoreWeight.java
@@ -1,4 +1,29 @@
 package com.WanSoo.entity.university;
 
-public class ScoreWeight {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class ScoreWeight { //학과별 수능 반영 비율 관련 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int korean;
+    private int math;
+    private int english;
+    private int social;
+    private int science;
+    private int history;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
 }
+
