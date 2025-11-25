53455db9419c53f3b476e6076a5252d0ef6ce486
Hyun-38 <1226johnny@gmail.com>
2025-11-25 15:30:06 +0900

    Feat(#1) AdmissionCut 엔티티 작성

--- src/main/java/com/WanSoo/entity/university/AdmissionCut.java
+++ src/main/java/com/WanSoo/entity/university/AdmissionCut.java
@@ -1,4 +1,25 @@
 package com.WanSoo.entity.university;
 
-public class AdmissionCut {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class AdmissionCut { //연도별 합격 컷 구현 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int year;
+
+    @Column(nullable = false)
+    private double cutScore;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
 }
