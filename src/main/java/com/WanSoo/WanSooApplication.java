f0b9047869a7b729ab7fac7e259e86d85157751f
Hyun-38 <1226johnny@gmail.com>
2025-11-25 09:43:40 +0900

    Backend 초기 설정

---
+++ src/main/java/com/WanSoo/WanSooApplication.java
@@ -0,0 +1,13 @@
+package com.WanSoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WanSooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WanSooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WanSoo/WanSooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WanSoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WanSooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
