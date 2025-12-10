5c15930c946afbbe476f2376aaa2f2272a89f903
Hyun-38 <1226johnny@gmail.com>
2025-12-10 21:19:20 +0900

    Feat(#1) config 파일 패키지 구성

--- src/main/java/com/WanSoo/config/JpaConfig.java
+++ src/main/java/com/WanSoo/config/JpaConfig.java
@@ -1,4 +1,12 @@
 package com.WanSoo.config;
 
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing
 public class JpaConfig {
+// 필요하면 AuditingDate 등을 설정
 }
