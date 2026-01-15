b893809030918692177a3bae18aa554b3f6ee100
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:42:09 +0900

    Feat(#2) UserScoreRequest 클래스 생성

--- src/main/java/com/WanSoo/dto/user/UserScoreRequest.java
+++ src/main/java/com/WanSoo/dto/user/UserScoreRequest.java
@@ -1,4 +1,29 @@
 package com.WanSoo.dto.user;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UserScoreRequest {
+
+    @Min(0) @Max(100)
+    private double korean;
+
+    @Min(0) @Max(100)
+    private double math;
+
+    @Min(0) @Max(100)
+    private double english;
+
+    @Min(0) @Max(100)
+    private double social;
+
+    @Min(0) @Max(100)
+    private double science;
+
+    @Min(0) @Max(100)
+    private double history;
 }
