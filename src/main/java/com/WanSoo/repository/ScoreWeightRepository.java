8e2a832452f504c47fec544b7ca509d3201efb46
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:34:42 +0900

    Feat(#2) ScoreWeightRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/ScoreWeightRepository.java
+++ src/main/java/com/WanSoo/repository/ScoreWeightRepository.java
@@ -1,4 +1,15 @@
 package com.WanSoo.repository;
 
-public interface ScoreWeightRepository {
+import com.WanSoo.entity.university.ScoreWeight;
+import com.WanSoo.entity.university.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ScoreWeightRepository extends JpaRepository<ScoreWeight, Long> {
+
+    /**
+     * 학과별 수능 반영 비율 조회
+     */
+    Optional<ScoreWeight> findByDepartment(Department department);
 }
