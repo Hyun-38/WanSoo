ed94d61faaf3bddfe2176f9f7cc88e41244b66e8
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:40:37 +0900

    Feat(#2) UserResponse 클래스 생성

--- src/main/java/com/WanSoo/dto/user/UserResponse.java
+++ src/main/java/com/WanSoo/dto/user/UserResponse.java
@@ -1,4 +1,23 @@
 package com.WanSoo.dto.user;
 
+import com.WanSoo.entity.user.User;
+import com.WanSoo.entity.user.UserRole;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class UserResponse {
+
+    private Long id;
+    private String email;
+    private UserRole role;
+
+    public static UserResponse from(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .role(user.getRole())
+                .build();
+    }
 }
