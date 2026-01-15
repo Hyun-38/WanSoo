b9ae3be7afe5ab43a2d3e01ffc82442f4952c5cf
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:34:33 +0900

    Feat(#2) DepartmentRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/DepartmentRepository.java
+++ src/main/java/com/WanSoo/repository/DepartmentRepository.java
@@ -1,4 +1,24 @@
 package com.WanSoo.repository;
 
-public interface DepartmentRepository {
+import com.WanSoo.entity.university.Department;
+import com.WanSoo.entity.university.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    /**
+     * 특정 대학의 학과 목록 조회
+     */
+    List<Department> findByUniversity(University university);
+
+    /**
+     * 대학 + 학과명으로 단건 조회
+     */
+    Optional<Department> findByUniversityAndName(
+            University university,
+            String name
+    );
 }
