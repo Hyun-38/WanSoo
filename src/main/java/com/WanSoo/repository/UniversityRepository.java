6427a847a64e3858982206f4cd233e38e3b88ba6
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:35:41 +0900

    Feat(#2) UniversityRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/UniversityRepository.java
+++ src/main/java/com/WanSoo/repository/UniversityRepository.java
@@ -1,4 +1,19 @@
 package com.WanSoo.repository;
 
-public interface UniversityRepository {
+import com.WanSoo.entity.university.University;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UniversityRepository extends JpaRepository<University, Long> {
+
+    /**
+     * 지역 기반 대학 조회
+     */
+    List<University> findByRegion(String region);
+
+    /**
+     * 대학명 포함 검색
+     */
+    List<University> findByNameContainingIgnoreCase(String name);
 }
