370acbbc40fb631cb242260f6a1d88e1d84a25d0
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:33:58 +0900

    Feat(#2) MockApplyRequest 클래스 생성

--- src/main/java/com/WanSoo/dto/university/MockApplyRequest.java
+++ src/main/java/com/WanSoo/dto/university/MockApplyRequest.java
@@ -1,4 +1,17 @@
 package com.WanSoo.dto.university;
 
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class MockApplyRequest {
+
+    @NotNull
+    private Long universityId;
+
+    @NotNull
+    private Long departmentId;
+    private double score;
 }
