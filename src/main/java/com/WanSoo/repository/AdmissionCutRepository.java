1f375f5b8ceb9dea283922aac6dca1be88dcecbf
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:32:34 +0900

    Feat(#2) AdmissionCutRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/AdmissionCutRepository.java
+++ src/main/java/com/WanSoo/repository/AdmissionCutRepository.java
@@ -1,4 +1,17 @@
 package com.WanSoo.repository;
 
-public interface AdmissionCutRepository {
+import com.WanSoo.entity.university.AdmissionCut;
+import com.WanSoo.entity.university.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AdmissionCutRepository extends JpaRepository<AdmissionCut, Long> {
+
+    /**
+     * 학과별 최신 합격 컷 조회
+     */
+    Optional<AdmissionCut> findTopByDepartmentOrderByYearDesc(
+            Department department
+    );
 }
