1ddcaccc29bce2cf48622ce3f03f66321728f82a
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:40:31 +0900

    Feat(#2) UserLoginRequest 클래스 생성

--- src/main/java/com/WanSoo/dto/user/UserLoginRequest.java
+++ src/main/java/com/WanSoo/dto/user/UserLoginRequest.java
@@ -1,4 +1,25 @@
 package com.WanSoo.dto.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UserLoginRequest {
+
+    /**
+     * 로그인 이메일
+     */
+    @Email
+    @NotBlank
+    private String email;
+
+    /**
+     * 평문 비밀번호 (요청용)
+     * → Service에서 암호화 비교
+     */
+    @NotBlank
+    private String password;
 }
