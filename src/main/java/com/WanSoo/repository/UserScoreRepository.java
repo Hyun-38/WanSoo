222e6806bec2c918257b6af037fa86ab21e97f44
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:36:26 +0900

    Feat(#2) UserScoreRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/UserScoreRepository.java
+++ src/main/java/com/WanSoo/repository/UserScoreRepository.java
@@ -1,4 +1,25 @@
 package com.WanSoo.repository;
 
-public interface UserScoreRepository {
+import com.WanSoo.entity.user.User;
+import com.WanSoo.entity.user.UserScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserScoreRepository extends JpaRepository<UserScore, Long> {
+
+    /**
+     * 사용자 기준 점수 조회
+     */
+    Optional<UserScore> findByUser(User user);
+
+    /**
+     * 사용자 ID 기준 점수 조회
+     */
+    Optional<UserScore> findByUserId(Long userId);
+
+    /**
+     * 사용자 점수 존재 여부
+     */
+    boolean existsByUser(User user);
 }
