1fdd81bc5a6e26f1a8466cd7fff8c090a6657c12
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:39:40 +0900

    Feat(#2) UniversityResponse 클래스 생성

--- src/main/java/com/WanSoo/dto/university/UniversityResponse.java
+++ src/main/java/com/WanSoo/dto/university/UniversityResponse.java
@@ -1,4 +1,15 @@
 package com.WanSoo.dto.university;
 
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class UniversityResponse {
+
+    private Long universityId;
+    private String universityName;
+    private String departmentName;
+    private String region;
+    private double admissionScore;
 }
