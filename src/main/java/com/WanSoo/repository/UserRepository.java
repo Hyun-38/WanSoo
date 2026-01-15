78ff41ab778bdb19c3476142d69bbadf037ff254
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:34:55 +0900

    Feat(#2) UserRepository 레포지토리 생성

--- src/main/java/com/WanSoo/repository/UserRepository.java
+++ src/main/java/com/WanSoo/repository/UserRepository.java
@@ -1,4 +1,19 @@
 package com.WanSoo.repository;
 
-public interface UserRepository {
+import com.WanSoo.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * 이메일로 사용자 조회 (로그인 / 인증)
+     */
+    Optional<User> findByEmail(String email);
+
+    /**
+     * 이메일 존재 여부 확인 (회원가입 중복 체크)
+     */
+    boolean existsByEmail(String email);
 }
