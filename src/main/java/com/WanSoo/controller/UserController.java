b5b405dd46e20ebf1a9e67ce3e1267a3f0797b8c
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:40:22 +0900

    Feat(#2) UserController 생성

--- src/main/java/com/WanSoo/controller/UserController.java
+++ src/main/java/com/WanSoo/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.WanSoo.controller;
 
+import com.WanSoo.dto.user.UserResponse;
+import com.WanSoo.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    /**
+     * 👤 내 정보 조회
+     */
+    @GetMapping("/me")
+    public ResponseEntity<UserResponse> myInfo(
+            @AuthenticationPrincipal User user
+    ) {
+        return ResponseEntity.ok(UserResponse.from(user));
+    }
 }
