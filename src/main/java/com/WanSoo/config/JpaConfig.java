d2c95a11827477f8bffd84d94d35157f54a3dfc4
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:33:10 +0900

    Feat(#2) JpaConfig 클래스 생성

--- src/main/java/com/WanSoo/config/JpaConfig.java
+++ src/main/java/com/WanSoo/config/JpaConfig.java
@@ -3,10 +3,12 @@ package com.WanSoo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 @Configuration
 @EnableJpaAuditing
+@EnableTransactionManagement
 public class JpaConfig {
 // 필요하면 AuditingDate 등을 설정
 }
