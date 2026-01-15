d8f0d70b3169b78e60debafd28802c10c2ad3204
Hyun-38 <1226johnny@gmail.com>
2026-01-16 01:39:47 +0900

    Feat(#2) UniversitySearchRequest 클래스 생성

--- src/main/java/com/WanSoo/dto/university/UniversitySearchRequest.java
+++ src/main/java/com/WanSoo/dto/university/UniversitySearchRequest.java
@@ -1,4 +1,13 @@
 package com.WanSoo.dto.university;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
 public class UniversitySearchRequest {
+
+    private String universityName;
+    private String departmentName;
+    private String region;
 }
